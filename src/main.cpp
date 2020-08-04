55ead09165f35a682e0901d1eceab1ca5e39cb5a
Ali Abdul-Kareem <aliabdul-kareem@Alis-MacBook-Air.local>
2020-08-04 13:41:29 +0300

    Testing the source code GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,37 @@
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-void processInput (GLFWwindow* window);
-int main()
-{
-    //Initialization and setting the version and mode.
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //only for macOS we need to enable forward_Compatiblity
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    //creating a window from GLFW
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    //creating the context (which is the current OpenGL state).
+
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    //checking if GLAD is initialized correctly.
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    //specifying how much W & H of the window we created we want for rendering.
-    glViewport(0, 0, 800, 600);
-
-
-    //closing the window on clicking.
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        processInput (window);
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
-
-    //Release/Clean all the buffers that is stored for GLFW.
     glfwTerminate();
     return 0;
-}
-
-
-//processing Input from Keyboard
-void processInput   (GLFWwindow* window)
-{
-    //if pressing escape, close that window
-    if (glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window,true);
-
-}
+    }
